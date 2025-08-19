e003b301c74120b433b9bb3ad0ea0de7c2c28edb
Unlagable <engqwerty43@gmail.com>
2025-08-19 20:10:07 +0700

    mobile responsive

--- src/components/explore acet/Explore ACET.jsx
+++ src/components/explore acet/Explore ACET.jsx
@@ -6,7 +6,7 @@ function ACETCard ({text, img}) {
     return(
         <div className='flex items-center flex-col justify-start p-4 gap-4 bg-neutral-200'>
             <img src={img} alt="" style={{height: '250px', width: '100%', objectFit:'cover'}}/>
-            <h2 className='text-header-2 text-accent'>{text}</h2>
+            <h2 className='text-header-2 text-accent uppercase'>{text}</h2>
         </div>
     )
 }
@@ -16,9 +16,9 @@ export default function ExploreACET () {
         <div className="Explore-Previous-ACET-Section px-4 bg-white pb-4">
             <h1 className="text-header-1 py-4 text-primary">Explore Previous ACET</h1>
             <div className="grid grid-rows-3 gap-4 md:grid-cols-3 md:grid-rows-1">
-                <ACETCard text="2024" img={acet2024}/>
-                <ACETCard text="2023" img={acet2023}/>
-                <ACETCard text="2022" img={acet2022}/>
+                <ACETCard text="ACET 2024" img={acet2024}/>
+                <ACETCard text="ACET 2023" img={acet2023}/>
+                <ACETCard text="ACET 2022" img={acet2022}/>
             </div>
         </div>
     )
